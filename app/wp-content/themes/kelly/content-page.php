d7bb96a2b0c2387c01b9fe047a11ba94d38e60dd
Eli Morris-Heft <eli.morris.heft@gmail.com>
2017-03-18 21:13:12 -0500

    Add featured images to content pages

--- app/wp-content/themes/kelly/content-page.php
+++ app/wp-content/themes/kelly/content-page.php
@@ -7,6 +7,11 @@
 ?>
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+	<?php if ( has_post_thumbnail() ) : ?>
+		<div class="entry-image">
+			<a href="<?php the_permalink(); ?>" rel="bookmark"><?php the_post_thumbnail( 'kelly-featured-image' ); ?></a>
+		</div>
+	<?php endif; ?>
 	<header class="entry-header">
 		<h1 class="entry-title"><?php the_title(); ?></h1>
 	</header><!-- .entry-header -->
