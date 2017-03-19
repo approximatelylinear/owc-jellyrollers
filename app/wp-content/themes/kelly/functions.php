eaf5fef4f2cb39509b4cbe07a9d829670554ec58
Chris Alvarado <calvarado@jellyvision.com>
2017-03-19 03:57:26 -0500

    [135] Create donate button short code

--- app/wp-content/themes/kelly/functions.php
+++ app/wp-content/themes/kelly/functions.php
@@ -180,6 +180,11 @@ function kelly_admin_scripts( $hook_suffix ) {
 }
 add_action( 'admin_print_styles-appearance_page_custom-header', 'kelly_admin_scripts' );
 
+add_shortcode('donate_button', 'donate_button');
+function donate_button() {
+	echo '<a href="https://squareup.com/store/chicago-baseball-cancer-charity" target="_blank"><button>Donate</button></a>';
+}
+
 /**
  * Implement the Custom Header feature.
  */
